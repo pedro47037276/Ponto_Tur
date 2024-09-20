27cfbbbb6d9fd0db80c745c1207526414efae299
Allan Souza <allan46884806@edu.pa.senac.br>
2024-09-20 09:03:07 -0300

    Parte de criar rota

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/* DESCOMENTAR DEPOIS
 var map = L.map('map').setView([0, 0], 19);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
@@ -24,19 +24,19 @@ var map = L.map('map').setView([0, 0], 19);
         ]).addTo(map);*/
 
         /* Texto ao clicar no icone localizador*/
-        marker.bindPopup("<b>Olá</b><br>Você está aqui").openPopup();
+        //marker.bindPopup("<b>Olá</b><br>Você está aqui").openPopup();     <--- DESCOMENTAR DEPOIS
         /* Texto ao clicar no císculo vermelho*/
         /*circle.bindPopup("I am a circle.");*/
         /*texto ao clicar triângulo */
         /*polygon.bindPopup("I am a polygon.");*/
 
-        /*var popup = L.popup()
+        /*var popup = L.popup()                      <--- DESCOMENTAR DEPOIS
         .setLatLng([-1.448517, -48.491839])
         .setContent("Localização atual")
         .openOn(map);*/
 
 
-        var popup = L.popup();
+        //var popup = L.popup();
 
         /*function onMapClick(e) {
             popup
@@ -49,8 +49,9 @@ map.on('click', onMapClick);*/
 
 
 
+// CONDIÇÃO DE LOCALIZAÇÃO
 
-if (navigator.geolocation) {
+/*if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
@@ -67,4 +68,47 @@ if (navigator.geolocation) {
     });
 } else {
     alert('Geolocalização não é suportada pelo seu navegador.');
+}*/
+
+
+
+
+// CÓDIGO NOVO 
+
+function success(pos){
+    //console.log(pos.coords.latitude, pos.coords.longitude);
+
+    // centralizar mapa
+    var map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 20);
+    
+    if (map === undefined){
+        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 20);
+    } else{
+        map.remove();
+        map = L.map('map').setView([pos.coords.latitude, pos.coords.longitude], 20);
+    }
+
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+
+    // CRIAR ROTA
+    L.Routing.control({
+        waypoints: [
+        L.latLng(pos.coords.latitude, pos.coords.longitude),
+        L.latLng(-1.4560646164723035, -48.501267232480984)
+        ]
+    }).addTo(map);
+
+    // criar e apontar icone do local
+    var marker = L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map);
+    marker.bindPopup("<p>Você está aqui</p>").openPopup();
+
+   
 }
+
+
+
+navigator.geolocation.getCurrentPosition(success);
