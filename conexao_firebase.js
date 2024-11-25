da68d8fee31815dce2d571f5dad55330af8d30b0
Allan Souza <allan46884806@edu.pa.senac.br>
2024-11-25 11:03:27 -0300

    Adiciona arquivo de conexão com banco e separa código de pesquisa

---
+++ conexao_firebase.js
@@ -0,0 +1,13 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCI_287E4XUbQ_fyhO_tyJ72FKRkGZ8hLI",
+    authDomain: "ponto-tur-5e4db.firebaseapp.com",
+    projectId: "ponto-tur-5e4db",
+    storageBucket: "ponto-tur-5e4db.appspot.com",
+    messagingSenderId: "1093552150779",
+    appId: "1:1093552150779:web:bbe89f5f8643414166385e"
+  };
+  
+  // Inicialize o Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
