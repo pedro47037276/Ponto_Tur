fa6394c206289bc663d672719f1beff4b59c27d2
pedro47037276 <pedro47037276@edu.pa.senac.br>
2024-12-12 01:53:39 -0300

    mostra user logado na aba de conta

--- java/conexao_firebase.js
+++ java/conexao_firebase.js
@@ -1,3 +1,6 @@
+// Importa a função initializeApp do módulo Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
 // Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyCI_287E4XUbQ_fyhO_tyJ72FKRkGZ8hLI",
@@ -6,8 +9,9 @@ const firebaseConfig = {
     storageBucket: "ponto-tur-5e4db.appspot.com",
     messagingSenderId: "1093552150779",
     appId: "1:1093552150779:web:bbe89f5f8643414166385e"
-  };
-  
-  // Inicialize o Firebase
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
+};
+
+// Inicializa o Firebase App
+const app = initializeApp(firebaseConfig);
+
+export default app; // Exporta o app para uso em outros arquivos
