1e38b5e3dc2f6ea89dc6671c0e9bd54945088165
Allan Souza <allan46884806@edu.pa.senac.br>
2025-01-22 11:29:47 -0300

    Adiciona um pouco de estilo nos serviços

--- java/servicos.js
+++ java/servicos.js
@@ -35,18 +35,30 @@ export function mostrar_servicos(ptservicos) {
         const div = document.createElement('div');
         div.className = 'servico-item';
 
+        const icon = document.createElement('a');
+        icon.href = '#';
+        icon.innerHTML = '<i class="bi bi-shop-window"></i>';
+        icon.className = 'icon_serv';
+        div.appendChild(icon);
+
+        const div_infos = document.createElement('div');
+        div_infos.className = 'div_infos';
+        div.appendChild(div_infos);
+
         const h5 = document.createElement('h5');
         h5.innerText = servico.nome;
-        div.appendChild(h5);
+        h5.className = 'servico_nome';
+        div_infos.appendChild(h5);
 
-        const p = document.createElement('p');
-        p.innerText = servico.descricao;
-        div.appendChild(p);
+        // const p = document.createElement('p');
+        // p.innerText = servico.descricao;
+        // div.appendChild(p);
 
         if (servico.funcionamento) {
             const funcionamento = document.createElement('p');
             funcionamento.innerText = `Funcionamento: ${servico.funcionamento}`;
-            div.appendChild(funcionamento);
+            funcionamento.className = 'horario'
+            div_infos.appendChild(funcionamento);
         }
 
         sidebar_serv.appendChild(div);
